c07df01e0718d2ee246310bd53df3f24b854c80e
BattleSkorpion <darian040904@gmail.com>
2022-12-23 23:12:55 -0500

    ioexception

--- src/settings/LocalizationSettings.java
+++ src/settings/LocalizationSettings.java
@@ -1,5 +1,15 @@
 package settings;
 
+import java.io.*;
+
 public class LocalizationSettings {
-    
+    File settings_file = new File("src\\settings\\localization_settings");
+    FileWriter settingsWriter = new FileWriter(settings_file, true);		// true = append
+    BufferedWriter settingsBWriter = new BufferedWriter(settingsWriter);
+    PrintWriter settingsPWriter = new PrintWriter(settingsBWriter); 		        // for println syntax
+
+
+    public LocalizationSettings() throws IOException {
+
+    }
 }
