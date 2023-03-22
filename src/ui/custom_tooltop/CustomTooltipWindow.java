c8730c5ae8b402fee4d1664574913adcc9d09fe5
BattleSkorpion <darian040904@gmail.com>
2023-03-22 16:47:30 -0400

    update

---
+++ src/ui/custom_tooltop/CustomTooltipWindow.java
@@ -0,0 +1,16 @@
+package ui.custom_tooltop;
+
+import javax.swing.*;
+
+public class CustomTooltipWindow extends JFrame {
+    private JPanel CustomTooltipWindowJPanel;
+
+    public CustomTooltipWindow() {
+        setContentPane(CustomTooltipWindowJPanel);
+        setSize(700, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+
+
+    }
+}
