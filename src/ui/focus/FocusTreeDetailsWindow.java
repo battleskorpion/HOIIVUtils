20a9f43dd88e443671af54ad2391127a7d37d672
BattleSkorpion <darian040904@gmail.com>
2023-07-02 11:12:07 -0400

    focus details focus id

--- src/ui/focus/FocusTreeDetailsWindow.java
+++ src/ui/focus/FocusTreeDetailsWindow.java
@@ -1,13 +1,22 @@
 package ui.focus;
 
+import clausewitz_coding.focus.Focus;
+
 import javax.swing.*;
 
 public class FocusTreeDetailsWindow extends JFrame {
-    private JLabel countryNameJLabel;
+    private JLabel focusNameJLabel;
     private JPanel focusTreeDetailsJPanel;
 
-    public FocusTreeDetailsWindow() {
+    Focus focus;
+
+    public FocusTreeDetailsWindow(Focus focus) {
         super ("Details");
+
+        if (focus == null) {
+            System.err.println("Focus null in " + this);
+            return;
+        }
 
         /* window */
         setContentPane(focusTreeDetailsJPanel);
@@ -15,6 +24,6 @@ public class FocusTreeDetailsWindow extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         pack();
 
-        
+        focusNameJLabel.setText(focus.id());
     }
 }
