0e6a7c3fecb78c5f6f0a4522501f9d976e1e4d8c
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2023-08-17 22:22:10 -0600

    update SettingsWindow.fxml and SettingsWindowController.java

--- src/ui/menu/SettingsWindowController.java
+++ src/ui/menu/SettingsWindowController.java
@@ -1,23 +1,28 @@
 package ui.menu;
 
+import java.awt.TextField;
+
 import clausewitz_coding.HOI4Fixes;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 
 public class SettingsWindowController {
     
     @FXML
     public CheckBox devModeCheckBox;
+    public Label hoi4ModFolderLabel;
+    public Button browseButton;
+    public TextField hoi4ModFolderTextField;
     public Button okButton;
     
-    public void openMenu() {
-        HOI4Fixes.closeWindow(okButton);
-        HOI4Fixes.openMenu();
-    }
-
     public void devMode() {
 
     }
 
+    public void openMenu() {
+        HOI4Fixes.closeWindow(okButton);
+        HOI4Fixes.openMenu();
+    }
 }
