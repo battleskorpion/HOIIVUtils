d64d4ab47a0f0bb0dde4e414a79228d250d5420b
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2023-08-17 19:30:04 -0600

    update launch.json and Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,22 @@
 import clausewitz_coding.HOI4Fixes;
+import hellofx.Main;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Launcher {
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws RuntimeException,IOException {
+        String whatToRun;
+        System.out.print("Press 1 and Enter to test test\nPress anything and enter to run normally:\n");
+        Scanner scanner = new Scanner(System.in);
+        whatToRun = scanner.nextLine();
+        scanner.close();
+        System.out.print("What you chose:\n " + whatToRun + "\n");
+        if (whatToRun == "1") {
+            Main.main(args);
+        }
+        else {
             HOI4Fixes.main(args);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
