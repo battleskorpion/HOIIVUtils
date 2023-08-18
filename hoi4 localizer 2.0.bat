367c945998b8170e2497c146e6e512343ed5c311
BattleSkorpion <darian040904@gmail.com>
2023-08-18 02:24:27 -0400

    users can run the program now

--- hoi4 localizer 2.0.bat
+++ hoi4 localizer 2.0.bat
@@ -1,1 +1,1 @@
-java -jar hoi4-localizer-2.0.jar :: USE .bat to run the localizer !!!!!!!!
+java --module-path "lib\javafx-sdk-20.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar out\artifacts\HOIIVUtils.jar :: USE .bat to run the localizer !!!!!!!!
