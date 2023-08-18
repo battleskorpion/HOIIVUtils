9e8044b36040a4851749fa0b0e898d9dea27c181
BattleSkorpion <darian040904@gmail.com>
2023-08-18 00:37:44 -0400

    Update SettingsWindowController.java

--- src/ui/menu/SettingsWindowController.java
+++ src/ui/menu/SettingsWindowController.java
@@ -1,17 +1,20 @@
 package ui.menu;
 
 import java.awt.TextField;
-import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
 import clausewitz_coding.HOI4Fixes;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.stage.DirectoryChooser;
 import javafx.scene.control.Button;
+import settings.HOIIVUtilsProperties;
 
 public class SettingsWindowController {
-    
+
+    HashMap<HOIIVUtilsProperties.Settings, String> settings;
+
     @FXML
     public CheckBox devModeCheckBox;
     public Label hoi4ModFolderLabel;
@@ -30,5 +33,21 @@ public class SettingsWindowController {
     public void openMenu() {
         HOI4Fixes.closeWindow(okButton);
         HOI4Fixes.openMenu();
+    }
+
+    public void saveSettings() {
+        try {
+            if (HOI4Fixes.firstTimeSetup) {
+                HOI4Fixes.settings = new HOIIVUtilsProperties(settings);
+            } else {
+                HOIIVUtilsProperties.saveSettings(settings);
+            }
+        } catch (IOException e) {
+            HOI4Fixes.openError(e);
+        }
+    }
+
+    public void tempUpdateSetting(HOIIVUtilsProperties.Settings setting, String property) {
+        settings.put(setting, property);
     }
 }
