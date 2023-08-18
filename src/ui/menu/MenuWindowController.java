7c30f773ee64ed1417fa306b9aad8175c2502f8c
BattleSkorpion <darian040904@gmail.com>
2023-08-17 20:20:14 -0400

    openSettings

--- src/ui/menu/MenuWindowController.java
+++ src/ui/menu/MenuWindowController.java
@@ -1,9 +1,9 @@
 package ui.menu;
 
-import static clausewitz_coding.HOI4Fixes.launchSettings;
+import static clausewitz_coding.HOI4Fixes.openSettings;
 
 public class MenuWindowController {
     public void openSettings() {
-        launchSettings();
+        openSettings();
     }
 }
