b468b1bb4effe7ad1804b617d122877c4bec0c8e
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2023-08-17 21:09:44 -0600

    update MenuWindow.java

--- src/ui/menu/MenuWindow.java
+++ src/ui/menu/MenuWindow.java
@@ -6,12 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MenuWindow extends Application {
+public class MenuWindow {
 
-    public void start(Stage primaryStage) throws Exception {
+    private MenuWindow menuWindow;
+
+     public MenuWindow() {
+        menuWindow = this;
+    }
+
+    public void open() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MenuWindow.fxml"));
-        primaryStage.setTitle("Menu");
-        primaryStage.setScene((new Scene(root, 600, 400)));
-        primaryStage.show();
+        Stage stage = new Stage();
+        stage.setTitle("Hello World");
+        stage.setScene((new Scene(root, 600, 400)));
+        stage.show();
     }
 }
