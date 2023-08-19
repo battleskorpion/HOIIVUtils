c7e67fb9806d4cb69e0e0345a92b0819e032a72d
BattleSkorpion <darian040904@gmail.com>
2023-08-19 01:34:04 -0400

    push

--- src/ui/main_menu/MenuWindowController.java
+++ src/ui/main_menu/MenuWindowController.java
@@ -1,7 +1,16 @@
 package ui.main_menu;
 
+import clausewitz_coding.focus.Focus;
+import clausewitz_coding.focus.FocusTree;
+import hoi4utils.HOIIVUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import ui.focus_localization.FocusLocalizationWindow;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MenuWindowController {
 
@@ -18,5 +27,33 @@ public class MenuWindowController {
     public void openStatistics() {
         StatisticsWindow statisticsWindow = new StatisticsWindow();
         statisticsWindow.open();
+    }
+
+    public void openLocalizeFocusTree() {
+        File selectedDirectory;
+        try{
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            Stage stage = (Stage) (focusLocalizButton.getScene().getWindow());
+            selectedDirectory = directoryChooser.showDialog(stage);
+
+            if (selectedDirectory == null) {
+                HOIIVUtils.openError("Selected directory was null.");
+                return;
+            }
+
+        }
+        catch(Exception exception) {
+            HOIIVUtils.openError(exception);
+            return; 
+        }
+
+        try {
+            FocusTree focusTree = new FocusTree(selectedDirectory);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        FocusLocalizationWindow localizationWindow = new FocusLocalizationWindow();
+        localizationWindow.open();
     }
 }
