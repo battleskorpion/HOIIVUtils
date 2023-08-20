7f238dd6a31eca2517899b9607fad8222b6d36d1
BattleSkorpion <darian040904@gmail.com>
2023-08-19 23:04:03 -0400

    improve window

--- src/ui/main_menu/HOIUtilsWindow.java
+++ src/ui/main_menu/HOIUtilsWindow.java
@@ -7,9 +7,34 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public abstract class HOIUtilsWindow {
+	public String fxmlResource;
+	public String title;
 	Stage primaryStage;
 
+	public void open() {
+		try {
+			if (primaryStage != null) {
+				primaryStage.show();
+			} else {
+				Parent root = FXMLLoader.load(getClass().getResource(fxmlResource));
+
+				Stage primaryStage = new Stage();
+
+				primaryStage.setTitle(title);
+				primaryStage.setScene((new Scene(root)));
+
+				HOIIVUtils.decideScreen(primaryStage);
+				primaryStage.show();
+			}
+		} catch (Exception exception) {
+			HOIIVUtils.openError(exception);
+		}
+	}
+
 	public void open(String fxmlResource, String title) {
+		this.fxmlResource = fxmlResource;
+		this.title = title;
+
 		try {
 			if (primaryStage != null) {
 				primaryStage.show();
--- src/ui/main_menu/StatisticsWindow.java
+++ src/ui/main_menu/StatisticsWindow.java
@@ -8,7 +8,12 @@ import javafx.stage.Stage;
 
 public class StatisticsWindow extends HOIUtilsWindow {
 
+	public StatisticsWindow() {
+		fxmlResource = "Statistics.fxml";
+		title = "Statistics";
+	}
+
 	public void open() {
-		super.open("Statistics.fxml", "Statistics");
+		super.open();
 	}
 }
