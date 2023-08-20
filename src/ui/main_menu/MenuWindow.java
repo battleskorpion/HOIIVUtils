a2f53f9fba2626cbca0219bd3b78d57826010f75
BattleSkorpion <darian040904@gmail.com>
2023-08-19 21:38:59 -0400

    fix

--- src/ui/main_menu/MenuWindow.java
+++ src/ui/main_menu/MenuWindow.java
@@ -6,30 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MenuWindow {
+public class MenuWindow extends hoiUtilsWindow {
 	Stage primaryStage;
 
 	public void open(){
-		try {
-			if (primaryStage != null) {
-				primaryStage.show();
-			}
-			else {
-				Parent root = FXMLLoader.load(getClass().getResource("MenuWindow.fxml"));
-				
-				Stage primaryStage = new Stage();
-
-				primaryStage.setTitle("HOIIVUtils Menu");
-
-				primaryStage.setScene((new Scene(root)));
-
-				HOIIVUtils.decideScreen(primaryStage);
-				
-				primaryStage.show();
-			}
-		}
-		catch (Exception exception) {
-			HOIIVUtils.openError(exception);
-		}
+		super.open("MenuWindow.fxml");
 	}
 }
