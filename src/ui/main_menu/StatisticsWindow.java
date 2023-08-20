75c473000e74123f5076cfff833ebf6ae7247197
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2023-08-19 21:23:36 -0600

    update settings.json, MenuWindow.java and StatisticsWindow.java

--- src/ui/main_menu/StatisticsWindow.java
+++ src/ui/main_menu/StatisticsWindow.java
@@ -1,11 +1,5 @@
 package ui.main_menu;
 
-import hoi4utils.HOIIVUtils;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 public class StatisticsWindow extends HOIUtilsWindow {
 
 	public void open() {
