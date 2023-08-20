b4609d59853c7a297d0503cdfbf90b48ff4783d9
SlothMcShain <billyty.lanier@yahoo.com>
2023-08-19 22:11:03 -0500

    stuff

--- src/ui/focus_localization/FocusLocalizationWindow.java
+++ src/ui/focus_localization/FocusLocalizationWindow.java
@@ -1,10 +1,5 @@
 package ui.focus_localization;
 
-import hoi4utils.HOIIVUtils;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import ui.main_menu.HOIUtilsWindow;
 
 public class FocusLocalizationWindow extends HOIUtilsWindow {
