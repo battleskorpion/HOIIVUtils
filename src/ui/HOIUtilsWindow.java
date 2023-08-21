e26da830cc6cd7b6559a492e5dc24deb43de10d4
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2023-08-20 18:55:38 -0600

    update HOIIVUtils.jar, HOIUtilsWindow.java and SettingsWindow.java

--- src/ui/HOIUtilsWindow.java
+++ src/ui/HOIUtilsWindow.java
@@ -1,6 +1,7 @@
 package ui;
 
 import hoi4utils.HOIIVUtils;
+import hoi4utils.Settings;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -33,7 +34,9 @@ public abstract class HOIUtilsWindow {
 				primaryStage.setScene(scene);
 
 				/* style */
-				System.out.println(new File(styleSheetURL).getAbsolutePath());
+				if (Settings.DEV_MODE.enabled()) {
+					System.out.println(new File(styleSheetURL).getAbsolutePath());
+				}
 				scene.getStylesheets().add(styleSheetURL);
 
 				HOIIVUtils.decideScreen(primaryStage);
