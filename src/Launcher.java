277b81eb02229cbddf1712a2889130ea2bbf6d5a
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2023-08-24 20:38:11 -0600

    update 3 files and delete 4 files

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,20 +1,8 @@
-import gui.hellofx.Main;
 import hoi4utils.HOIIVUtils;
 import java.io.IOException;
 
 public class Launcher {
 	public static void main(String[] args) throws RuntimeException,IOException {
-		String whatToRun;
-//		System.out.print("Press 1 and Enter to test test\nPress anything and enter to run normally:\n");
-//		Scanner scanner = new Scanner(System.in);
-//		whatToRun = scanner.nextLine();
-//		scanner.close();
-		whatToRun = "";
-		if (whatToRun == "1") {
-			Main.main(args);
-		}
-		else {
-			HOIIVUtils.main(args);
-		}
+		HOIIVUtils.main(args);
 	}
 }
