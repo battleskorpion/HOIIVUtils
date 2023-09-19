60f96012093623f4eb1a07d52e3006e538966a55
BattleSkorpion <darian040904@gmail.com>
2023-09-19 12:46:10 -0400

    test

--- src/test/hoi4utils/clausewitz_coding/focus/FocusTest.java
+++ src/test/hoi4utils/clausewitz_coding/focus/FocusTest.java
@@ -1,11 +1,25 @@
 package test.hoi4utils.clausewitz_coding.focus;
 
+import hoi4utils.clausewitz_coding.focus.Focus;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 public class FocusTest {
 
 	@Test
-	public void setDescLocalization_updatesFocusLocFile() {
+	public void testFocusProperties() {
+		Focus focus = mock();
+		when(focus.id()).thenReturn("ALA_test_focus");
+		when(focus.nameLocalization()).thenReturn("ALA_test_focus:0 \"Test Focus\"");
+		when(focus.descLocalization()).thenReturn("ALA_test_focus_desc:0 \"\"");
+
+		
+	}
+
+	@Test
+	public void testSetDescLocalization() {
 
 	}
+
+
 }
