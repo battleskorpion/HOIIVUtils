fb79a5c46cb3665997bd7db7d393dec5403c6534
Juddur <judsonftw@gmail.com>
2023-10-14 22:02:39 -0500

    Update AATUpdateUtils.java

--- src/hoi4utils/util/AATUpdateUtils.java
+++ src/hoi4utils/util/AATUpdateUtils.java
@@ -1,5 +1,23 @@
 package hoi4utils.util;
 
+import java.io.File;
+
+import hoi4utils.HOIIVFile;
+import hoi4utils.clausewitz_parser.Expression;
+import hoi4utils.clausewitz_parser.Parser;
+
 public class AATUpdateUtils {
-	
+    double MULTIPLIER = 3.00 ;
+	public void fixconsumergoodsfactor () {
+        File ideadirectory = HOIIVFile.ideas_folder ;
+        for ( File ideaFile: ideadirectory.listFiles () ) {
+            Parser parser = new Parser ( ideaFile );
+            Expression[] exp = parser.expression().getAllSubexpressions ( "consumer_goods_factor=" );
+            for ( Expression simon:exp ) {
+               double v = simon.getDoubleValue() ;
+                v *= MULTIPLIER;
+                
+            }
+        }
+    }
 }
