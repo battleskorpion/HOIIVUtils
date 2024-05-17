21ac55ea8ecca1afe64b1a4e4176d3c79305aadb
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2024-05-17 15:14:18 -0600

    update launch.json and HOIIVFile.java

--- src/main/java/com/HOIIVUtils/hoi4utils/HOIIVFile.java
+++ src/main/java/com/HOIIVUtils/hoi4utils/HOIIVFile.java
@@ -15,12 +15,14 @@ import java.io.IOException;
  * A Everything to do with the files that are found in a HOI 4 mod
  * - Paths
  * - Creating Files and Directories
- * ? <insert about whatever the file watchers do> chris learning what is a file watcher
+ * ? <insert about whatever the file watchers do> chris learning what is a file
+ * watcher
  * - Future things
  */
 public class HOIIVFile implements FileUtils {
 
-	public static final File usersParadoxHOIIVModFolder = new File(File.separator + "Paradox Interactive" + File.separator + "Hearts of Iron IV" + File.separator + "mod");
+	public static final File usersParadoxHOIIVModFolder = new File(
+			File.separator + "Paradox Interactive" + File.separator + "Hearts of Iron IV" + File.separator + "mod");
 
 	public static final String modPath = SettingsManager.get(Settings.MOD_PATH);
 
@@ -36,23 +38,22 @@ public class HOIIVFile implements FileUtils {
 	public static File common_folder;
 	public static File hoi4mods_folder;
 
-
 	public static void createHOIIVFilePaths() {
 		String modPath = SettingsManager.get(Settings.MOD_PATH);
 		System.out.println("modPath: " + modPath);
 
 		if (modPath == null) {
 			MessageController window = new MessageController();
-            window.open("Error: modPath was null and we can't create any files");
-            return;
+			window.open("Error: modPath was null and we can't create any files");
+			return;
 		}
 
 		File modPathFile = new File(modPath);
-		
+
 		common_folder = new File(modPath + "\\common");
 		states_folder = new File(modPath + "\\history\\states");
-		strat_region_dir =  new File(modPath + "\\map\\strategicregions");
-		localization_eng_folder =  new File(modPath + "\\localisation\\english");
+		strat_region_dir = new File(modPath + "\\map\\strategicregions");
+		localization_eng_folder = new File(modPath + "\\localisation\\english");
 		focus_folder = new File(modPath + "\\common\\national_focus");
 		ideas_folder = new File(modPath + "\\common\\ideas");
 		hoi4mods_folder = modPathFile.getParentFile();
@@ -60,11 +61,11 @@ public class HOIIVFile implements FileUtils {
 	}
 
 	/**
-	* Watch the state files in the given directory.
-	*
-	* @param stateFiles The directory containing state files.
-	* @throws IOException If an I/O error occurs.
-	*/
+	 * Watch the state files in the given directory.
+	 *
+	 * @param stateFiles The directory containing state files.
+	 * @throws IOException If an I/O error occurs.
+	 */
 	public static void watchStateFiles(File stateFiles) {
 		if (stateFiles == null || !stateFiles.exists() || !stateFiles.isDirectory()) {
 			System.err.println("State dir does not exist or is not a directory: " + stateFiles);
@@ -72,11 +73,11 @@ public class HOIIVFile implements FileUtils {
 		}
 
 		stateFilesWatcher = new FileWatcher(stateFiles);
-		
+
 		stateFilesWatcher.addListener(new FileAdapter() {
 			@Override
 			public void onCreated(FileEvent event) {
-//				System.out.println("State created in states dir");
+				// System.out.println("State created in states dir");
 				// todo building view thing
 				EventQueue.invokeLater(() -> {
 					stateFilesWatcher.listenerPerformAction++;
@@ -92,7 +93,7 @@ public class HOIIVFile implements FileUtils {
 
 			@Override
 			public void onModified(FileEvent event) {
-//				System.out.println("State modified in states dir");
+				// System.out.println("State modified in states dir");
 				// todo building view thing
 				EventQueue.invokeLater(() -> {
 					stateFilesWatcher.listenerPerformAction++;
@@ -108,7 +109,7 @@ public class HOIIVFile implements FileUtils {
 
 			@Override
 			public void onDeleted(FileEvent event) {
-//				System.out.println("State deleted in states dir");
+				// System.out.println("State deleted in states dir");
 				// todo building view thing
 				EventQueue.invokeLater(() -> {
 					stateFilesWatcher.listenerPerformAction++;
