53196d4422d0ac112907d88da191285eca102c14
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2024-05-17 23:18:19 -0600

    chore: Update Maven plugins and dependencies

---
+++ src/test/java/com/HOIIVUtils/hoi4utils/HOIIVUtilsTest.java
@@ -0,0 +1,12 @@
+package com.HOIIVUtils.hoi4utils;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue; // Import the assertTrue method
+
+class HOIIVUtilsTest {
+	@Test
+	void testMain() {
+		// Add an assertion to this test case
+		assertTrue(true);
+	}
+}
