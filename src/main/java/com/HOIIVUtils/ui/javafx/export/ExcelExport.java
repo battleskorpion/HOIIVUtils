0b3f03b94e5288df7b22a2e4423d1240036b3f9e
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2024-05-19 06:24:51 -0600

    update ExcelExport.java

--- src/main/java/com/HOIIVUtils/ui/javafx/export/ExcelExport.java
+++ src/main/java/com/HOIIVUtils/ui/javafx/export/ExcelExport.java
@@ -11,27 +11,28 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * adapted from <a href="https://stackoverflow.com/a/58074839/15639400">stackoverflow.com/a/58074839/15639400</a>
+ * adapted from <a href=
+ * "https://stackoverflow.com/a/58074839/15639400">stackoverflow.com/a/58074839/15639400</a>
+ * 
  * @param <T>
  */
 public class ExcelExport<T> {
-	public void export(TableView<T> tableView) {        // todo have one with String workBookName
+	public void export(TableView<T> tableView) { // todo have one with String workBookName
 		String workbookName = "HOIIVUtils Sheet" + LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE);
 
 		HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
 		HSSFSheet hssfSheet = hssfWorkbook.createSheet(workbookName);
 		HSSFRow firstRow = hssfSheet.createRow(0);
 
-		///set titles of columns
-		for (int i=0; i<tableView.getColumns().size();i++) {
+		/// set titles of columns
+		for (int i = 0; i < tableView.getColumns().size(); i++) {
 			firstRow.createCell(i).setCellValue(tableView.getColumns().get(i).getText());
 		}
 
+		for (int row = 0; row < tableView.getItems().size(); row++) {
+			HSSFRow hssfRow = hssfSheet.createRow(row + 1);
 
-		for (int row=0; row<tableView.getItems().size();row++){
-			HSSFRow hssfRow= hssfSheet.createRow(row + 1);
-
-			for (int col=0; col<tableView.getColumns().size(); col++){
+			for (int col = 0; col < tableView.getColumns().size(); col++) {
 				Object cellValue = tableView.getColumns().get(col).getCellObservableValue(row).getValue();
 
 				if (cellValue != null) {
@@ -49,6 +50,12 @@ public class ExcelExport<T> {
 			hssfWorkbook.write(fileOut);
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				hssfWorkbook.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
