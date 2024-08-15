b76951e21187bcdda587921a482ba24520316cc2
BattleSkorpion <darian040904@gmail.com>
2024-08-15 14:37:37 -0400

    Create FocusTreeTest.scala

---
+++ src/test/scala/com/hoi4utils/clausewitz/data/focus/FocusTreeTest.scala
@@ -0,0 +1,8 @@
+package com.hoi4utils.clausewitz.data.focus
+
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class FocusTreeTest extends AnyFunSuiteLike {
+
+  // todo have testable focus tree in test resources
+}
