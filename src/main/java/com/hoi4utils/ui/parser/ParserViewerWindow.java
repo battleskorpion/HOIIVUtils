b958b2363bd55cdc59639e4359decec94939434a
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2024-08-15 03:10:23 -0600

    update 6 files

--- src/main/java/com/hoi4utils/ui/parser/ParserViewerWindow.java
+++ src/main/java/com/hoi4utils/ui/parser/ParserViewerWindow.java
@@ -1,19 +1,29 @@
 package com.hoi4utils.ui.parser;
 
+import com.hoi4utils.clausewitz.HOIIVUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import com.hoi4utils.ui.HOIIVUtilsWindow;
 
 public class ParserViewerWindow extends HOIIVUtilsWindow {
+
+	@FXML
+	public Label idVersion;
+	@FXML
+	public Label idWindowName;
+	
 	public ParserViewerWindow() {
-		/* window */
 		setFxmlResource("ParserViewerWindow.fxml");
 		setTitle("HOIIVUtils Parser Viewer");
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@FXML
 	void initialize() {
+		includeVersion();
+		idWindowName.setText("StatisticsWindow" + " WIP");
+	}
+
+	private void includeVersion() {
+		idVersion.setText(HOIIVUtils.HOIIVUTILS_VERSION);
 	}
 }
