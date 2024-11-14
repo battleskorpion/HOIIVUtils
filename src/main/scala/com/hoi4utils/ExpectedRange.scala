8a89b0cf235171aba444bf103b02096bb46f4862
BattleSkorpion <darian040904@gmail.com>
2024-11-14 17:05:56 -0500

    Update ExpectedRange.scala

--- src/main/scala/com/hoi4utils/ExpectedRange.scala
+++ src/main/scala/com/hoi4utils/ExpectedRange.scala
@@ -22,9 +22,7 @@ class ExpectedRange[T <: AnyVal](val min: T, val max: T) {
 }
 
 object ExpectedRange {
-  def ofInt:
-    ExpectedRange[Int] = new ExpectedRange[Int](Int.MinValue, Int.MaxValue)
-    System.out.println("ccp")
+  def ofInt: ExpectedRange[Int] = new ExpectedRange[Int](Int.MinValue, Int.MaxValue)
 
   def ofDouble: ExpectedRange[Double] = new ExpectedRange[Double](Double.MinValue, Double.MaxValue)
 
