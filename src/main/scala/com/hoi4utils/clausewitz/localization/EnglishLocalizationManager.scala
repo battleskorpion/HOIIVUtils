c754d2e7f8f22924ef3a7f63b48efe10366a6198
BattleSkorpion <darian040904@gmail.com>
2025-02-12 23:00:15 -0500

    (NOT FINISHED) updating EnglishLocalizationManager to Scala

---
+++ src/main/scala/com/hoi4utils/clausewitz/localization/EnglishLocalizationManager.scala
@@ -0,0 +1,307 @@
+package com.hoi4utils.clausewitz.localization
+
+import com.hoi4utils.FileUtils
+import com.hoi4utils.clausewitz.HOIIVFile
+
+import scala.Option
+import scala.jdk.javaapi.CollectionConverters
+import java.io.*
+import java.nio.file.Files
+import java.nio.file.Paths
+import scala.collection.mutable.ListBuffer
+
+object EnglishLocalizationManager {
+  protected val l_english = "l_english:"
+  protected val versionNumberRegex = ":(\\d*)" //  (version number is optional)
+
+  protected val language_def: String = l_english
+}
+
+class EnglishLocalizationManager extends LocalizationManager with FileUtils {
+  /* */ 
+  setManager(this)
+  
+  /**
+   * a record is final, so this is fine. The key will always be the same as the localization key
+   * from when it is initially put in the map.
+   */
+  final protected val localizationCollection = new LocalizationCollection
+
+  //    public static void setLanguage(String language) {
+  //        language_def = "l_" + language;
+  //    }
+  override def reload(): Unit = {
+    localizationCollection.clear()
+    // load all localization files within the localization folder and
+    // any subfolders
+    loadLocalization()
+  }
+
+  protected def loadLocalization(): Unit = {
+    // load mod localization after vanilla to give mod localizations priority
+    if (HOIIVFile.hoi4_localization_folder == null) 
+      System.out.println("'HOI4 localization folder' null.")
+    else if (!HOIIVFile.hoi4_localization_folder.exists) 
+      System.out.println("'HOI4 localization folder' does not exist.")
+    else if (!HOIIVFile.hoi4_localization_folder.isDirectory) 
+      System.out.println("'HOI4 localization folder' is not a directory.")
+    else 
+      loadLocalization(HOIIVFile.mod_localization_folder, Localization.Status.EXISTS)
+    
+    if (HOIIVFile.mod_localization_folder == null) 
+      System.out.println("'Mod localization folder' null.")
+    else if (!HOIIVFile.mod_localization_folder.exists) 
+      System.out.println("'Mod localization folder' does not exist.")
+    else if (!HOIIVFile.mod_localization_folder.isDirectory) 
+      System.out.println("'Mod localization folder' is not a directory.")
+    else 
+      loadLocalization(HOIIVFile.hoi4_localization_folder, Localization.Status.VANILLA)
+  }
+
+  protected def loadLocalization(localizationFolder: File, status: Localization.Status): Unit = {
+    val files = localizationFolder.listFiles
+    if (files == null) return
+    for (file <- files) {
+      if (file.isDirectory) loadLocalization(file, status)
+      else if (file.getName.endsWith(".yml")) loadLocalizationFile(file, status)
+      else System.out.println("Localization files can only be of type .yml. File: " + file.getAbsolutePath)
+    }
+  }
+
+  protected def loadLocalizationFile(file: File, Status: Localization.Status): Unit = {
+    try {
+      val scanner = new Scanner(file)
+      try {
+        // check language
+        var languageFound = false
+        while (scanner.hasNextLine && !languageFound) {
+          var line = scanner.nextLine
+          /* ignore BOM */
+          if (line.startsWith("\uFEFF")) line = line.substring(1)
+          if (!FileUtils.usefulData(line)) continue //todo: continue is not supported
+          if (!line.trim.startsWith(EnglishLocalizationManager.language_def)) {
+            // todo we would have optional logging
+            System.out.println("Localization file is not in English: " + file.getAbsolutePath)
+            return
+          }
+          else languageFound = true
+        }
+        if (!languageFound) {
+          System.out.println("Localization file does not have a language definition: " + file.getAbsolutePath)
+          return
+        }
+        while (scanner.hasNextLine) {
+          val line = scanner.nextLine
+          if (!FileUtils.usefulData(line)) continue //todo: continue is not supported
+          val data = line.splitWithDelimiters(EnglishLocalizationManager.versionNumberRegex, 2)
+          if (data.length != 3) {
+            System.err.println("Invalid localization file format: " + file.getAbsolutePath + "\n\tline: " + line + "\n\tReason: incorrect number of line elements")
+            continue //todo: continue is not supported
+
+          }
+          // trim whitespace
+          for (i <- 0 until data.length) {
+            data(i) = data(i).trim
+          }
+          // ignore ":" before version number
+          data(1) = data(1).substring(1)
+          // ignore escaped quotes
+          data(2) = data(2).replaceAll("//\"", "\u0000")
+          val startQuote = data(2).indexOf("\"")
+          val endQuote = data(2).lastIndexOf("\"")
+          val extra = data(2).substring(endQuote + 1).trim
+          if (extra.nonEmpty && !extra.startsWith("#")) {
+            System.err.println("Invalid localization file format: " + file.getAbsolutePath + "\n\tline: " + line + "\n\tReason: extraneous non-comment data after localization entry: " + extra)
+            continue //todo: continue is not supported
+          }
+          if (startQuote != 0 || endQuote == -1 || startQuote == endQuote) {
+            System.err.println("Invalid localization file format: " + file.getAbsolutePath + "\n\tline: " + line + "\n\tReason: localization value is not correctly enclosed in quotes")
+            continue //todo: continue is not supported
+
+          }
+          // remove leading/trailing quotes (and any comments)
+          data(2) = data(2).substring(startQuote + 1, endQuote)
+          /*
+          .yml example format:
+          CONTROLS_GREECE: "Controls all states in the §Y$strategic_region_greece$§! strategic region"
+          CONTROLS_ASIA_MINOR:1 "Controls all states in the §Y$strategic_region_asia_minor$§! strategic region"
+          */
+          var key: String = null
+          var version: Integer = null
+          var value: String = null
+          if (data(1).isBlank) {
+            key = data(0)
+            version = null
+            value = data(2)
+          }
+          else {
+            key = data(0)
+            version = data(1).toInt
+            value = data(2)
+          }
+          // fix file format issues (as it is a UTF-8 BOM file)
+          value = value.replaceAll("(Â§)", "§")
+          val localization = new Localization(key, version, value, Status)
+          localizationCollection.add(localization, file)
+        }
+      } catch {
+        case exc: IOException =>
+          exc.printStackTrace()
+      } finally if (scanner != null) scanner.close()
+    }
+  }
+
+  override def saveLocalization(): Unit = {
+    val localizationFolder = HOIIVFile.mod_localization_folder
+    val files = localizationFolder.listFiles
+    if (files == null) return
+    // Separate new and changed localizations
+    val changedLocalizations = CollectionConverters.asJava(localizationCollection.filterByStatus(Localization.Status.UPDATED))
+    val newLocalizations = CollectionConverters.asJava(localizationCollection.filterByStatus(Localization.Status.NEW))
+    // Save updated and new localizations
+    changedLocalizations.forEach(entry => writeAllLocalization(CollectionConverters.asJava(entry._2.toList), entry._1))
+    newLocalizations.forEach(entry => writeAllLocalization(CollectionConverters.asJava(entry._2.toList), entry._1))
+  }
+
+  def writeAllLocalization(list: List[Localization], file: File): Unit = {
+    for (localization <- list) {
+      val key = localization.ID
+      val version = {
+        if (localization.version == null) ""
+        else String.valueOf(localization.version)
+      }
+      val value = localization.text
+      
+      localization.status match {
+        case UPDATED => writeLocalization(file, key, version, value, false)
+        case NEW => writeLocalization(file, key, version, value, true)
+        case _ => throw new IllegalStateException("Unexpected value: " + localization.status)
+      }
+    }
+  }
+
+  /**
+   * Use to replace existing localization with entry
+   *
+   * @param file
+   * @param key
+   * @param version
+   * @param value
+   * @param append
+   */
+  protected def writeLocalization(file: File, key: String, version: String, value: String, append: Boolean): Unit = {
+    var entry = key + ":" + version + " \"" + value + "\""
+    entry = entry.replaceAll("§", "Â§") // necessary with UTF-8 BOM
+
+    val writer: PrintWriter = getLocalizationWriter(file, true)
+    if (append) try {
+      writer.println(entry)
+    } catch {
+      case exc: IOException =>
+        System.err.println("Failed to write new localization to file. " + "\n\tLocalization: " + entry + "\n\tFile: " + file.getAbsolutePath)
+    } finally {
+      if (writer != null) writer.close()
+    }
+    else try {
+      var lineReplaced = false
+      val lines = Files.readAllLines(Paths.get(file.getAbsolutePath))
+      for (i <- 0 until lines.size) {
+        var continue = true
+        if (lines.get(i).trim.startsWith(key) && continue) {
+          lines.set(i, entry)
+          lineReplaced = true
+          System.out.println("Replaced localization " + key)
+          continue = false 
+        }
+      }
+      if (!lineReplaced) throw new IOException // todo better exception
+      Files.write(Paths.get(file.getAbsolutePath), lines)
+    } catch {
+      case exc: IOException =>
+        System.err.println("Failed to update localization in file. " + "\n\tLocalization: " + entry + "\n\tFile: " + file.getAbsolutePath)
+    }
+  }
+
+  @throws[IOException]
+  protected def getLocalizationWriter(file: File, append: Boolean) = new PrintWriter(new BufferedWriter(new FileWriter(file, append)))
+
+  /**
+   * {@inheritDoc }
+   */
+  @throws[LocalizationExistsException]
+  override def addLocalization(localization: Localization, file: File): Unit = {
+    if (localization == null) return
+    if (localizationCollection.containsLocalizationKey(localization.ID)) throw new LocalizationExistsException(localization)
+    localizationCollection.add(localization, file)
+  }
+
+  /**
+   * Finds the localization corresponding to the given key/localization ID.
+   *
+   * @return the localization corresponding to the given ID, or null if not found.
+   */
+  @throws[IllegalArgumentException]
+  override def getLocalization(key: String): Option[Localization] = {
+    if (key == null) throw new IllegalArgumentException("localization ID cannot be null")
+    localizationCollection.get(key)
+  }
+
+  /**
+   * Checks if the given localization ID is localized (has a localization entry).
+   *
+   * @param localizationId the ID of the localization to check
+   * @return true if the localization is localized, false otherwise
+   */
+  override def isLocalized(localizationId: String): Boolean = getLocalization(localizationId).nonEmpty
+  
+  override def localizations: LocalizationCollection = localizationCollection
+
+  /**
+   * Capitalizes every word in a string with a pre-set whitelist
+   *
+   * @param title
+   * @return Returns the edited string unless the string has no words
+   */
+  override def titleCapitalize(title: String): String = {
+    if (title == null) return null
+    if (title.trim.isEmpty) return title
+
+    val words = title.split(" ").toBuffer
+    val whitelist = capitalizationWhitelist
+    
+    if (words.head.length == 1) words(0) = Character.toUpperCase(words.head.charAt(0)) + ""
+    else if (words.head.length > 1) words(0) = Character.toUpperCase(words.head.charAt(0)) + words.head.substring(1)
+    else {
+      // todo this should never happen now right?
+      System.out.println("first word length < 1")
+    }
+    
+    System.out.println("num words: " + words.size)
+    for (i <- 1 until words.size) {
+      if (!isAcronym(words(i)) && !whitelist.contains(words.get(i))) {
+        if (words(i).length == 1) {
+          words(i) = Character.toUpperCase(words(i).charAt(0)) + ""
+        }
+        else if (words(i).length > 1) {
+          // System.out.println("working cap");
+          words(i) = Character.toUpperCase(words(i).charAt(0)) + words(i).substring(1)
+        }
+      }
+    }
+    
+    System.out.println("capitalized: " + String.join(" ", CollectionConverters.asJava(words)))
+    String.join(" ", CollectionConverters.asJava(words))
+  }
+
+  // todo let user change?
+  override def capitalizationWhitelist: util.HashSet[String] = {
+    val whitelist = Array("a", "above", "after", "among", // among us
+      "an", "and", "around", "as", "at", "below", "beneath", "beside", "between", "but", "by", "for", "from", "if", "in", "into", "nor", "of", "off", "on", "onto", "or", "over", "since", "the", "through", "throughout", "to", "under", "underneath", "until", "up", "with")
+    // create the whitelist
+    new util.HashSet[String](util.List.of(whitelist))
+  }
+
+  override def toString: String = "EnglishLocalizationManager{" + "localizations=" + localizationCollection + "}"
+
+  override def hashCode: Int = Objects.hash(localizationCollection)
+}
