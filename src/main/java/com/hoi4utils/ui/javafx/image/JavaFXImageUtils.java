a1304268b6e6a35a9d663e6ecb6aaba0a3d62e5d
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2025-02-26 15:31:43 -0700

    Update JavaFXImageUtils.java

--- src/main/java/com/hoi4utils/ui/javafx/image/JavaFXImageUtils.java
+++ src/main/java/com/hoi4utils/ui/javafx/image/JavaFXImageUtils.java
@@ -4,7 +4,19 @@ import javafx.scene.image.Image;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 
+/**
+ * Utility class for converting DDS pixel data into a JavaFX Image.
+ */
 public class JavaFXImageUtils {
+
+	/**
+	 * Converts an array of ARGB pixel data into a JavaFX {@link Image}.
+	 *
+	 * @param ddspixels the array of pixel data in ARGB format
+	 * @param ddswidth     the width of the image
+	 * @param ddsheight    the height of the image
+	 * @return a {@link Image} constructed from the provided pixel data
+	 */
 	public static Image imageFromDDS(int[] ddspixels, int ddswidth, int ddsheight) {
 		WritableImage writableImage = new WritableImage(ddswidth, ddsheight);
 		PixelWriter pixelWriter = writableImage.getPixelWriter();
