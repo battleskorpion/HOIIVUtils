b0bd61951f21055d8fc568cfa28e3194ec2685bd
BattleSkorpion <darian040904@gmail.com>
2025-03-02 13:33:23 -0500

    remove converted and unnecessary

--- src/main/java/com/hoi4utils/clausewitz/data/idea/ManpowerIdea.java
+++
@@ -1,9 +0,0 @@
-//package com.HOIIVUtils.hoi4utils.clausewitz_data.idea;
-///**
-// * This is the ManpowerIdea file.
-// */
-//public class ManpowerIdea extends Idea {
-//	public ManpowerIdea(String ideaID) {
-//		super(ideaID);
-//	}
-//}
