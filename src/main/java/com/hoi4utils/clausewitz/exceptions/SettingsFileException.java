7f0795f3937454236035aaab5745fda729fd04bb
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2025-03-03 13:38:24 -0700

    deleted unused exc

--- src/main/java/com/hoi4utils/clausewitz/exceptions/SettingsFileException.java
+++
@@ -1,9 +0,0 @@
-package com.hoi4utils.clausewitz.exceptions;
-
-import java.io.IOException;
-
-public class SettingsFileException extends RuntimeException {
-	public SettingsFileException(String message, IOException e) {
-		super(message, e);
-	}
-}
--- src/main/java/com/hoi4utils/clausewitz/exceptions/SettingsWriteException.java
+++
@@ -1,9 +0,0 @@
-package com.hoi4utils.clausewitz.exceptions;
-
-import java.io.IOException;
-
-public class SettingsWriteException extends RuntimeException {
-	public SettingsWriteException(String message, IOException e) {
-		super(message, e);
-	}
-}
