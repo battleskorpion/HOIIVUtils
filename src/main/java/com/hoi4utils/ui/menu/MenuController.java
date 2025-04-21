d558e27d3f4af819de2c24747247515f95bef745
BattleSkorpion <darian040904@gmail.com>
2025-04-20 22:41:05 -0400

    Revert "Update MenuController.java"
    This reverts commit 8bda1c3c4502493cb61f11daa92016495f29701f.

--- src/main/java/com/hoi4utils/ui/menu/MenuController.java
+++ src/main/java/com/hoi4utils/ui/menu/MenuController.java
@@ -126,19 +126,16 @@ public class MenuController extends Application implements JavaFXUIManager {
 				if (settingsValidationThread != null && settingsValidationThread.isAlive()) {
 					settingsValidationThread.interrupt(); // Interrupt the thread
 				}
-                if (dialog != null) {
-                    assert dialog != null;
-                    dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                        @Override
-                        public void windowClosing(java.awt.event.WindowEvent e) {
-                            LOGGER.info("Popup dialog closed.");
-                            isPopupClosed = true;
-                            if (!stage.isShowing()) {
-                                Platform.exit(); // Exit if the main menu is already closed
-                            }
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        LOGGER.info("Popup dialog closed.");
+                        isPopupClosed = true;
+                        if (!stage.isShowing()) {
+                            Platform.exit(); // Exit if the main menu is already closed
                         }
-                    });
-                }
+                    }
+                });
                 if (!isPopupClosed) {
                     Platform.exit();
                 }
