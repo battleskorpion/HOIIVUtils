4bac40751c0228d26dfafb29de924c0a71d4c753
Chris Cason <57425817+thiccchris@users.noreply.github.com>
2025-05-14 10:37:20 -0600

    Seperated Version Class

--- src/main/scala/com/hoi4utils/Updater.scala
+++ src/main/scala/com/hoi4utils/Updater.scala
@@ -87,21 +87,3 @@ class Updater {
     }
   }
 }
-
-case class Version(major: Int, minor: Int, patch: Int) extends Ordered[Version] {
-  override def compare(that: Version): Int =
-    Ordering[(Int, Int, Int)]
-      .compare((major, minor, patch), (that.major, that.minor, that.patch))
-
-  override def toString: String = s"$major.$minor.$patch"
-}
-
-object Version {
-  /** Parses strings like "1.2.3" (and will throw on malformed input) */
-  def apply(s: String): Version = {
-    if (s == null || s.isEmpty) throw new IllegalArgumentException("Version string cannot be null or empty")
-    val Array(a, b, c) = s.split("\\.", 3)
-    Version(a.toInt, b.toInt, c.toInt)
-  }
-  val DEFAULT: Version = Version(0, 0, 0)
-}
