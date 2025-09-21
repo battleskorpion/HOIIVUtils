03181d70cebdd74f002c2d75e6f34c0312b17453
thiccchris <57425817+thiccchris@users.noreply.github.com>
2025-09-20 18:00:59 -0600

    Update CountryTag.scala

--- src/main/scala/com/hoi4utils/hoi4/country/CountryTag.scala
+++ src/main/scala/com/hoi4utils/hoi4/country/CountryTag.scala
@@ -5,10 +5,39 @@ import com.hoi4utils.{HOIIVFiles, PDXReadable}
 import com.typesafe.scalalogging.LazyLogging
 
 import java.io.{File, IOException}
+import java.nio.file.Files
 import scala.collection.mutable.ListBuffer
+import scala.io.Source
+import scala.util.Using
+
+/**
+ *
+ * @param tag the country tag (generally a 3 letter code)
+ * @param file if this tag is defined in the mod's (or vanilla's) country tags files, file it is defined in,
+ *             or should be defined in.
+ */
+class CountryTag private(val tag: String, var file: Option[File] = None) extends Comparable[CountryTag] {
+  CountryTag.addTag(this)
+
+  def get: String = tag
+  
+  override def toString: String = {
+    tag
+  }
+  
+  override def equals(obj: Any): Boolean = obj match
+    case other: CountryTag => tag == other.tag
+    case str: String => tag == str
+    case _ => false
+
+  override def compareTo(o: CountryTag): Int = {
+    tag.compareTo(o.tag)
+  }
+
+  def isFileDefined: Boolean = file.isDefined
+}
 
 object CountryTag extends Iterable[CountryTag] with LazyLogging with PDXReadable {
-
   val NULL_TAG: CountryTag = CountryTag("###")
   val COUNTRY_TAG_LENGTH = 3 // standard country tag length (for a normal country tag)private final String tag;
   // scala... (this is null (????????????) if you dont use 'lazy')
@@ -40,18 +69,16 @@ object CountryTag extends Iterable[CountryTag] with LazyLogging with PDXReadable
         rootNode.foreach(node => {
           CountryTag(node.name.trim, file)
         })
-        //        Using.resource(Source.fromFile(file)) { source =>
-        //          for (line <- source.getLines()) {
-        //            val data = line.replaceAll("\\s", "")
-        //            if (data.nonEmpty && data.charAt(0) != '#') {
-        //              val key = data.takeWhile(_ != '=').trim
-        //              val tag = CountryTag(key, file)
-        //              if (!tag.equals(CountryTag.NULL_TAG) && (!skipDuplicates || !tagsBuf.contains(tag))) {
-        //                tagsBuf += tag
-        //              }
-        //            }
-        //          }
-        //        }
+        Using.resource(Source.fromFile(file)) { source =>
+          source
+            .getLines()
+            .map(_.replaceAll("\\s", ""))
+            .filter(data => data.nonEmpty && data.head != '#')
+            .map(data => CountryTag(data.takeWhile(_ != '=').trim, file))
+            .filter(_ != CountryTag.NULL_TAG)
+            .filter(tag => !skipDuplicates || !tagsBuf.contains(tag))
+            .foreach(tagsBuf += _)
+        }
       }
     }
 
@@ -62,17 +89,18 @@ object CountryTag extends Iterable[CountryTag] with LazyLogging with PDXReadable
   }
 
   def apply(tag: String): CountryTag = _tagList.find(_.get == tag) match
-    case Some(countryTag) => countryTag
     case None => new CountryTag(tag)
+    case Some(countryTag) => countryTag
 
   def apply(tag: String, file: File): CountryTag = _tagList.find(t => t.get == tag) match
+    case None => new CountryTag(tag, Some(file))
     case Some(countryTag) =>
-      if (countryTag.file.isEmpty) countryTag.file = Some(file)
+      if (countryTag.file.isEmpty)
+        countryTag.file = Some(file)
       else if (countryTag.file.get != file)
         val message = s"Tag '$tag' from file '$file' already found in loaded tags, file: ${countryTag.file}"
         Country.countryErrors += message
       countryTag
-    case None => new CountryTag(tag, Some(file))
 
   override def iterator: Iterator[CountryTag] = _tagList.iterator
 
@@ -89,32 +117,4 @@ object CountryTag extends Iterable[CountryTag] with LazyLogging with PDXReadable
   def countryTags: Seq[CountryTag] = _tagList.toList
 
   def exists(tag: String): Boolean = _tagList.exists(_.equals(tag))
-
-}
-
-/**
- *
- * @param tag the country tag (generally a 3 letter code)
- * @param file if this tag is defined in the mod's (or vanilla's) country tags files, file it is defined in,
- *             or should be defined in.
- */
-class CountryTag private(val tag: String, var file: Option[File] = None) extends Comparable[CountryTag] {
-  CountryTag.addTag(this)
-
-  def get: String = tag
-  
-  override def toString: String = {
-    tag
-  }
-  
-  override def equals(obj: Any): Boolean = obj match
-    case other: CountryTag => tag == other.tag
-    case str: String => tag == str
-    case _ => false
-
-  override def compareTo(o: CountryTag): Int = {
-    tag.compareTo(o.tag)
-  }
-
-  def isFileDefined: Boolean = file.isDefined
 }
