fb6f84a606c464ee2f9e3b8f96e373fd74e6813d
BattleSkorpion <darian040904@gmail.com>
2025-11-09 08:31:08 -0500

    Update CollectionPDX.scala

--- src/main/scala/com/hoi4utils/script/CollectionPDX.scala
+++ src/main/scala/com/hoi4utils/script/CollectionPDX.scala
@@ -53,20 +53,17 @@ abstract class CollectionPDX[T <: PDXScript[?]](pdxSupplier: PDXSupplier[T], pdx
    */
   @throws[UnexpectedIdentifierException]
   @throws[NodeValueTypeException]
-  override protected def addToCollection(expression: Node): Unit = {
-    expression.$ match {
-      case l: ListBuffer[Node] =>
-        for (childExpr <- l) {
-          val childScript = useSupplierFunction(childExpr)
-          childScript.loadPDX(childExpr)
-          pdxList += childScript
-        }
-      case _ =>
-        val childScript = useSupplierFunction(expression)
-        childScript.loadPDX(expression)
+  override protected def addToCollection(expression: Node): Unit = expression.$ match
+    case l: ListBuffer[Node] =>
+      for (childExpr <- l) {
+        val childScript = useSupplierFunction(childExpr)
+        childScript.loadPDX(childExpr)
         pdxList += childScript
-    }
-  }
+      }
+    case _ =>
+      val childScript = useSupplierFunction(expression)
+      childScript.loadPDX(expression)
+      pdxList += childScript
 
   /**
    * Adds a PDXScript to the list of PDXScripts. Used for when the PDXScript is not loaded from a file.
@@ -74,53 +71,43 @@ abstract class CollectionPDX[T <: PDXScript[?]](pdxSupplier: PDXSupplier[T], pdx
    * @param pdxScript the PDXScript to add
    */
   @targetName("add")
-  def +=(pdxScript: T): Unit = {
-    pdxList += pdxScript
-  }
+  def +=(pdxScript: T): Unit = pdxList += pdxScript
 
   /**
    * Removes elements matching the predicate.
    * Also removes the corresponding node(s) from the underlying Node.
    */
-  def removeIf(p: T => Boolean): ListBuffer[T] = {
-    for (i <- pdxList.indices.reverse) { // iterate backwards for safe removal
-      if (p(pdxList(i))) {
-        pdxList.remove(i)
-        node match {
-          case Some(n) => n.remove(i)
-          case None      => // do nothing
-        }
-      }
-    }
+  def removeIf(p: T => Boolean): ListBuffer[T] =
+    for
+      i <- pdxList.indices.reverse
+      if p(pdxList(i))
+    do // iterate backwards for safe removal
+      pdxList.remove(i)
+      node.foreach(_.remove(i))
     pdxList
-  }
 
-  protected def useSupplierFunction(expression: Node): T = {
-    pdxSupplier(expression) match {
-      case Some(s) => s
-      case None => throw new UnexpectedIdentifierException(expression)
-    }
-  }
+  protected def useSupplierFunction(expression: Node): T = pdxSupplier(expression) match
+    case Some(s) => s
+    case None => throw new UnexpectedIdentifierException(expression)
+
 
   /**
    * Clears the underlying collection and, if present, clears the corresponding node list.
    */
-  def clear(): Unit = {
+  def clear(): Unit =
     pdxList.clear()
     node.foreach { n =>
-      n.$ match {
+      n.$ match
         case l: ListBuffer[?] => l.clear()
         case _ =>
-      }
     }
-  }
 
   override def isEmpty: Boolean = pdxList.isEmpty
 
   override def iterator: Iterator[T] = pdxList.iterator // todo idk
 
   override def foreach[U](f: T => U): Unit = super.foreach(f)
-  
+
   override def length: Int = pdxList.length
 
   override def apply(i: Int): T = pdxList(i)
@@ -129,12 +116,11 @@ abstract class CollectionPDX[T <: PDXScript[?]](pdxSupplier: PDXSupplier[T], pdx
 
   override def isUndefined: Boolean = pdxList.forall(_.isUndefined) || pdxList.isEmpty
 
-  override def set(expression: Node): Unit = {
+  override def set(expression: Node): Unit =
     usingIdentifier(expression)
     this.node = Some(expression)
     val value = expression.$
     setNode(value)
-  }
 
   // todo @skorp implement?
   override def set(obj: ListBuffer[T]): ListBuffer[T] = obj
