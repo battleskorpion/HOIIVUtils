49d4aae5e413922b6c761ed8118142a12ae6c0fe
BattleSkorpion <darian040904@gmail.com>
2026-02-15 14:01:14 -0500

    Update FocusTree.scala

--- src/main/scala/com/hoi4utils/hoi42/common/national_focus/FocusTree.scala
+++ src/main/scala/com/hoi4utils/hoi42/common/national_focus/FocusTree.scala
@@ -7,7 +7,15 @@ import scala.reflect.ClassTag
 class FocusTree extends Registry[Focus] with PDXEntity {
 
   val id = pdx[String]("id") required true
-  val focuses = pdxList[Focus]("focus")
+//  val country = pdx[FocusTreeCountry]
+  val focuses = pdxList[Focus]("focus") required true
+  val default = pdx[Boolean]("default")
+  val resetOnCivilWar = pdx[Boolean]("reset_on_civil_war")
+//  val continuousFocusPosition = pdx[PointPDX]("continuous_focus_position")
+//  val initialShowPosition = pdx[InitialShowPosition]("initial_show_position")
+//  val shortcut = pdx[Shortcut]("shortcut")
+//  /** special handling */ 
+//  val sharedFocuses = pdxList[Reference[SharedFocus]]("shared_focus")
 
 
   override def idDecoder: PDXDecoder[String] = summon[PDXDecoder[String]]
